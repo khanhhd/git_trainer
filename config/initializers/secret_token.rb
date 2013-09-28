898edf9a295f8a1e58e6744b08f28a299e028df4
Khanhhd <hoang.dang.khanh@framgia.com>
2013-09-28 14:39:15 +0700

    Init commit 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AppTutorial::Application.config.secret_key_base = '2e7fc521b4691e7e5e0a602a5b64c1b309dccfb0b1059c2041353d94eb601bc9aa1e14881ed83d40b7b565b9032e095684a41e01f76c93ea85ae136ba4391e8b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AppTutorial::Application.config.session_store :cookie_store, key: '_app_tutorial_session'
