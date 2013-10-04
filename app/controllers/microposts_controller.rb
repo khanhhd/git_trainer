1a4c11a74e4d8458c6efaa1d4b01aa4b46574e18
Khanhhd <hoang.dang.khanh@framgia.com>
2013-10-05 00:55:19 +0700

    unfollow and follow  with Ajax

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,6 +8,7 @@ class MicropostsController < ApplicationController
       flash[:success] = "Success created"
       redirect_to root_url
     else
+      @feed_items = []
       render "static_pages/home"
     end
   end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,8 +3,8 @@ require "spec_helper"
 describe "Static page" do
   describe "Home page" do
     before {visit root_path}
-    it "should have the content 'StaticPages#home'" do
-      expect(page).to have_content("StaticPages#home")
+    it "should have the content 'Welcome to the Sample App'" do
+      expect(page).to have_content("Welcome to the Sample App")
     end
     it "should have the title 'Ruby on Rails'" do
       expect(page).to have_title("Ruby on Rails")
