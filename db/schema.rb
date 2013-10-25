162fb5d02cc0d89177104709f14b0293e5fc0f9d
Truong My Hai Ninh <truong.my.hai.ninh@framgia.com>
2013-10-25 16:09:07 +0700

    refs #8274 Drop column phone number

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131003023553) do
+ActiveRecord::Schema.define(version: 20131025082439) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
