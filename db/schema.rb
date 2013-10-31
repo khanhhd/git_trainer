1d445d49b6bae11ab100672b95eb2382f1170175
Nguyen Duc Giang <nguyen.duc.giang@framgia.com>
2013-10-31 13:27:02 +0700

    ref #8496 Remove Phone column in User table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131025085811) do
+ActiveRecord::Schema.define(version: 20131031062400) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
@@ -42,7 +42,6 @@ ActiveRecord::Schema.define(version: 20131025085811) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "address"
-    t.string   "phone"
   end
 
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
