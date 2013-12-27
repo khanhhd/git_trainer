1b153e3178e359311c2ed2394b406e5dad7e3545
Dac Nguyen Minh <nguyen.minh.dac@framgia.com>
2013-12-27 16:04:31 +0700

    refs task#10208 change paignation

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if signed_in?
       @micropost = current_user.microposts.build if signed_in?
-      @feed_items = current_user.feed.paginate page: params[:page], per_page: 7
+      @feed_items = current_user.feed.paginate page: params[:page], per_page: 25
     end
   end
 end
