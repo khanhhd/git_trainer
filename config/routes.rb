630faf173b46c69ca867cfe93c46c7957867e619
quanghung <quanghung89bn@gmail.com>
2013-12-27 15:43:45 +0700

    refs task10210 Name of task

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ AppTutorial::Application.routes.draw do
   resources :relationships, only: [:create, :destroy]
   root  'static_pages#home'
   match '/about',   to: 'static_pages#about',   via: 'get'
+    match '/help',   to: 'static_pages#help',   via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
